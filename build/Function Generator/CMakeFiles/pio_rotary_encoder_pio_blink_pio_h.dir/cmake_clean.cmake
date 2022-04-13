70ca97d1b3fbba314511808d0ae29b4197205302
Tony <tony@oddwires.co.uk>
2022-04-13 21:02:45 +0100

    Delete build directory

--- build/Function Generator/CMakeFiles/pio_rotary_encoder_pio_blink_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pio_rotary_encoder_pio_blink_pio_h"
-  "pio_blink.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pio_rotary_encoder_pio_blink_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/Function Generator/CMakeFiles/pio_rotary_encoder_pio_rotary_encoder_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-﻿# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for pio_rotary_encoder_pio_rotary_encoder_pio_h.
